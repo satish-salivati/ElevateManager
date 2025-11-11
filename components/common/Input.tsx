22f2e71c10b7cbbd45603baf28e3633b9088a02d
Satish Salivati <salivatisatish@gmail.com>
2025-11-11 09:58:16 +0530

    Update Input.tsx

--- components/common/Input.tsx
+++ components/common/Input.tsx
@@ -2,19 +2,21 @@
 import React from 'react';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+  label?: string;
   id: string;
 }
 
-const Input: React.FC<InputProps> = ({ label, id, ...props }) => {
+const Input: React.FC<InputProps> = ({ label, id, className, ...props }) => {
   return (
     <div>
-      <label htmlFor={id} className="block text-sm font-medium text-slate-700 text-left">
-        {label}
-      </label>
+      {label && (
+        <label htmlFor={id} className="block text-sm font-medium text-slate-700 text-left">
+          {label}
+        </label>
+      )}
       <input
         id={id}
-        className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md shadow-sm placeholder-slate-400 focus:outline-none focus:ring-brand-primary focus:border-brand-primary sm:text-sm"
+        className={`${label ? 'mt-1' : ''} block w-full px-3 py-2 bg-white border border-slate-300 rounded-md shadow-sm placeholder-slate-400 focus:outline-none focus:ring-brand-primary focus:border-brand-primary sm:text-sm ${className || ''}`}
         {...props}
       />
     </div>
